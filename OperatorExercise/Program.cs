84ec8d4f59c423e104a68dde94845de3cc2c9371
Jonathan Borja <jborja.one@gmail.com>
2024-10-10 19:22:25 -0600

    finished exercesi 1 and 2

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+
+            int sum = a + b;
+            int difference = a - b;
+            int product = a * b;
+            int quotient = a / b;
+            int remainder = a % b;
+
+            Console.WriteLine($"The sum of {a} and {b} is {sum}");
+            Console.WriteLine($"The difference between {a} and {b} is {difference}");
+            Console.WriteLine($"The product of {a} and {b} is {product}");
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+            Console.WriteLine("Enter a radius for a circle:");
+            var radius = double.Parse(Console.ReadLine());
+
+            AreaOfCircle(radius);
+
+        }
+
+        static void AreaOfCircle(double radius){
+            double area = Math.PI * Math.Pow(radius, 2);
+            Console.WriteLine($"The area of a circle with radius {radius} is {area}");
         }
     }
 }
